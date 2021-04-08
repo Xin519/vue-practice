836c8ed39224f0676dbf22e8c836b05bf6b39bea
BLEACH <3027421343@qq.com>
2021-04-08 17:20:12 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,15 @@
   </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+export default class App extends Vue {
+}
+// export default Vue.extend({
+// })
+</script>
+
 <style lang="scss" scoped></style>
