3d99623f8f34d02eb436fb5add7760ae3c716375
BLEACH <3027421343@qq.com>
2021-04-27 17:05:36 +0800

    add

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,17 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/member-delimiter-style': ['error', {
+      multiline: {
+        delimiter: 'none',
+        requireLast: true
+      },
+      singleline: {
+        delimiter: 'semi',
+        requireLast: false
+      },
+      multilineDetection: 'brackets'
+    }]
   }
 }
